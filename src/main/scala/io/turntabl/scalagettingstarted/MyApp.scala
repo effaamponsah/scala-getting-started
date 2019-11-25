ac2f3164294c9e6db51b5e869b059850e54c6e5b
Dennis Effa <kamponsah.amponsah@gmail.com>
2019-11-25 11:25:59 +0000

    3-5 done

--- src/main/scala/io/turntabl/scalagettingstarted/MyApp.scala
+++ src/main/scala/io/turntabl/scalagettingstarted/MyApp.scala
@@ -1,11 +1,24 @@
 package io.turntabl.scalagettingstarted
 
 object MyApp extends App {
-        print("Yesss. Here i am")
+        print("Yesss. Here i am \n")
 //        1
 
         val name: String ="Dennis"
         val place: String ="Kumasi"
 
-        print(s"$name was born in $place")
+        print(s"$name was born in $place \n")
+
+//        3
+        def threeTimes(s : String) = for(a<-1 to 3) print(s)
+
+        threeTimes("James")
+
+//        4
+        def even(number:Int) = if(number % 2 == 0) true else false
+
+        even(2)
+
+//        5
+//        def odd (number:Int) =
 }
