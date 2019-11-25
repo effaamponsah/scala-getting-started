3decc34272fb799f3e92f233975247237a18b94a
Dennis Effa <kamponsah.amponsah@gmail.com>
2019-11-25 10:56:20 +0000

    number 1 done

--- src/main/scala/io/turntabl/scalagettingstarted/MyApp.scala
+++ src/main/scala/io/turntabl/scalagettingstarted/MyApp.scala
@@ -1,5 +1,11 @@
 package io.turntabl.scalagettingstarted
 
 object MyApp extends App {
-    print("Hello, World!")
+        print("Yesss. Here i am")
+//        1
+
+        val name: String ="Dennis"
+        val place: String ="Kumasi"
+
+        print(s"$name was born in $place")
 }
